9085013213771180822c3ed47dda07f7c6d6685a
Irina <38900844+LLevella@users.noreply.github.com>
2021-01-08 03:43:12 +0700

    Update settings.py

--- moviebase/moviebase/settings.py
+++ moviebase/moviebase/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0c(+)ee&&7n86ffpi3(-zr%o6f664e^il-w50$kofcysmm6!uq'
-
+SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
