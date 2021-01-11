ec2284c111fa63be222600073b23f9ed1fbf35b9
Llevella <irinavkhomutova@gmail.com>
2021-01-11 14:24:05 +0700

    bugs were fixed

--- moviebase/back/views.py
+++ moviebase/back/views.py
@@ -7,8 +7,7 @@ from .serializers import MovieListSerializer, MovieDetailSerializer,  MoviePlaye
 
 from django.conf import settings
 import redis
-# player_time = redis.StrictRedis(host=settings.REDIS_HOST,
-#                                   port=settings.REDIS_PORT, db=0)
+
 movie_user_pointer = redis.StrictRedis(host=settings.REDIS_HOST,
                                   port=settings.REDIS_PORT, db=0)
 
@@ -30,7 +29,6 @@ class MovieDetailView(APIView):
 
 class MoviePlayerView(APIView):
     """Добавление времени, обновление, получение времени по фильму и пользователю """
-    
     def post(self, request):
         serializer = MoviePlayerSerializer(data=request.data)
         if serializer.is_valid():
@@ -38,14 +36,14 @@ class MoviePlayerView(APIView):
             key_id = f'{movie_id}:{user_id}'
             pointer = serializer.data["pointer"]
             movie_user_pointer.set(key_id, pointer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"movie": movie_id, "user": user_id, "pointer": serializer.data['pointer']}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def get(self, request):
         movie_id = request.data.get("movie")
         user_id = request.data.get("user")
         key_id = f'{movie_id}:{user_id}'
-        pointer = redis_instance.get(key_id)
+        pointer = movie_user_pointer.get(key_id)
         if pointer:
             return Response({"movie": movie_id, "user": user_id, "pointer": pointer}, status=status.HTTP_200_OK)
         if MoviePlayer.objects.filter(movie=movie_id, user=user_id).exists():
@@ -59,11 +57,11 @@ class MoviePlayerView(APIView):
         user_id = request.data.get("user")
         new_pointer = request.data.get("pointer")
         key_id = f'{movie_id}:{user_id}'
-        old_pointer = redis_instance.get(key_id)
+        old_pointer = movie_user_pointer.get(key_id)
         if old_pointer:
-            redis_instance.set(key_id, new_pointer)
+            movie_user_pointer.set(key_id, new_pointer)
             return Response({"movie": movie_id, "user": user_id, "pointer": new_pointer}, status=status.HTTP_200_OK)
-        if MoviePlayer.objects.filter(id=player_id).exists():
-            redis_instance.set(key_id, new_pointer)
+        if MoviePlayer.objects.filter(movie=movie_id, user=user_id).exists():
+            movie_user_pointer.set(key_id, new_pointer)
             return Response({"movie": movie_id, "user": user_id, "pointer": new_pointer}, status=status.HTTP_200_OK)
         return Response({"errors": "Movies player is not found"}, status=status.HTTP_400_BAD_REQUEST)
