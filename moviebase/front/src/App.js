d4a2d1388e21d5b706f7145e330694c1fe995d69
Llevella <irinavkhomutova@gmail.com>
2021-01-13 18:07:32 +0700

    axio was added to send requests, test front vision was started

--- moviebase/front/src/App.js
+++ moviebase/front/src/App.js
@@ -1,43 +1,51 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import { Container, Row, Col, Navbar, Nav, Card, Button } from "react-bootstrap";
+import axios from 'axios';
+import { Container, Row, Col, Navbar, Nav, Card, } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { render } from "react-dom";
-// import MovieListDisplay from './movieList.js';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      activePage: 0,
+      films: []
     };
   } 
+
+  componentDidMount() {
+     axios.get('back/')
+       .then(res => {
+         console.log(res);
+         console.log(res.data);
+        const films = res.data;
+        this.setState({ films });
+      })
+  }
+
   render() {
     return (
       <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Фильмы</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Вход</Nav.Link>
-          <Nav.Link href="#pricing">Выход</Nav.Link>
-          <Nav.Link href="#pricing">Регистрация</Nav.Link>  
-        </Nav>
-      </Navbar>
-      <Container>
-        <Row>
-            <Col xs={6} md={4}>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#main">Фильмы</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#login">Вход</Nav.Link>
+            <Nav.Link href="#logout">Выход</Nav.Link>
+            <Nav.Link href="#register">Регистрация</Nav.Link>  
+          </Nav>
+        </Navbar>
+        <Container fluid="md">
+          <Row>
+            <Col>
               <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/280x420" />
+                <Card.Img variant="top" src="/media/images/300x450.webp" />
                 <Card.Body>
-                  <Card.Title></Card.Title>
-                  <Card.Text>
-                  </Card.Text>
-                  <Button variant="primary"></Button>
+                  <Card.Title>Фантастиеские Твари</Card.Title>
+                  <Card.Link href="#detail">Подробнее</Card.Link>
                 </Card.Body>
               </Card>
-            </Col>
-        </Row>
+              </Col>
+          </Row>
         </Container>
       </>
     );
